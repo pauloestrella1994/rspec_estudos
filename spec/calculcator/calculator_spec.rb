eaf159e070f9515b4e50b10b619d840a7ca18908
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-02-27 16:42:55 -0300

    Use subject to instantiate a class

--- spec/calculcator/calculator_spec.rb
+++ spec/calculcator/calculator_spec.rb
@@ -1,15 +1,15 @@
 require 'calculator'
 
-describe Calculator do
+describe 'Calculadora' do
+  subject(:calc) { Calculator.new() }
+
   context '#sum' do
     it 'with positive numbers' do
-      calc = Calculator.new
       result = calc.sum(5,7)
       expect(result).to eq(12)
     end
 
     it 'with negatives numbers' do
-      calc = Calculator.new
       result = calc.sum(-5,7)
       expect(result).to eq(2)  
     end
