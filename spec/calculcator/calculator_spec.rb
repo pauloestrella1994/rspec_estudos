e349dabc7f915f911d23cc91d6a7cac747c55829
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-02-27 17:20:01 -0300

    Use expect().not_to from rspec_expectations

--- spec/calculcator/calculator_spec.rb
+++ spec/calculcator/calculator_spec.rb
@@ -7,6 +7,8 @@ describe 'Calculadora', "Descreve o Teste" do
     it 'with positive numbers' do
       result = calc.sum(5,7)
       expect(result).to eq(12)
+      expect(result).not_to eq(11)
+      expect(result).not_to eq(10)
     end
 
     it 'with negatives numbers' do
