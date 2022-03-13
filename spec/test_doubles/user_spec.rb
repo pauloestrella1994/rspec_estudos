9fb7e5132928945e4b441a64df0657299b07a69f
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-03-13 18:44:01 -0300

    As null object

--- spec/test_doubles/user_spec.rb
+++ spec/test_doubles/user_spec.rb
@@ -4,4 +4,12 @@ describe 'Test Double' do
     allow(user).to receive_messages(name: 'Paulo')
     allow(user).to receive(:password).and_return('secret')
   end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object # every error message in the instance will be ignore
+    allow(user).to receive_messages(name: 'Paulo')
+    allow(user).to receive(:password).and_return('secret')
+
+    user.abc # user has not method abc, but it will be ignored by as_null_object
+  end
 end
