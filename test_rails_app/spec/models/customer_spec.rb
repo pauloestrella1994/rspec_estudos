89407c2bf347b129d71cdcf94e2d04de573c9915
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-03-18 16:40:20 -0300

    Fixtures

--- test_rails_app/spec/models/customer_spec.rb
+++ test_rails_app/spec/models/customer_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :customers
+
+  it 'Create a Customer' do
+    customer = customers(:paulo)
+
+    expect(customer.name).to eq("Paulo")
+    expect(customer.email).to eq("paulo@paulo.com")
+    expect(customer.full_name).to eq("Sr. Paulo")
+  end
 end
